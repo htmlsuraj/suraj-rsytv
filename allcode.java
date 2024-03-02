cad3ed3a6408bb66ff749af1db26b4f9ab44a005
htm-rsytv <157800070+htm-rsytv@users.noreply.github.com>
2024-03-02 06:50:46 +0530

    hj

---
+++ allcode.java
@@ -0,0 +1,112 @@
+<HTML>
+<p><h1><li>Code for this websites<br>code for html</li></h1></p>
+<h2><li>for home page</li></h2>
+
+<html>
+    <head>
+        <style>
+            p{
+                font-family: georgia,garamond,serif;
+            }
+        </style>
+        <body>
+                <head>
+                <title>Anime Website</title>
+                <link href="https://fonts.googleapis.com/
+                css2?family=Aclonica&display=swap" rel="stylesheet"> <link rel="stylesheet" type="text/css" href="style.css">
+                </head>
+                <body>
+                <header>
+                <div class="logo">
+                <a href="home.html"><h1>AnimeWorld</h1></a>
+                </div>
+                <div class="nav_bar">
+                <ul>
+                <li><a href="#">all code</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="story mode.html">story mode</a></li>
+                <li><a href="contract.html">contract</a></1i>
+                <li><a href="aboutminecraft.html">minecraft command</a></1i>
+                </ul>
+				
+	
+                </div>
+                </header>
+			
+
+
+                </body>
+				<div class="video-slider">
+					<video src="anime.mp4" muted autoplay loop></video>
+				
+				</div>
+
+                </html>  
+        </body>
+
+        <h1><li>for stylecss in home screen</h1> </li>
+    
+
+
+        video {
+            width: 100%;
+          height: 100%;
+          min-height: 100%;
+            display:flex;
+            height: 125%;
+            background-size: cover;
+          }
+          *{
+              margin: 0;
+          padding: 0;
+          }
+          header{
+          height: 100px;
+          background: rgba(175, 11, 216, 0.7);
+          width: 100%;
+          }
+          .logo{
+          font-family: 'Aclonica';
+          font-size: 25px
+          padding 50px;
+          margin-left: 50px;
+          }
+          .logo a{
+          color: #009954;
+          text-decoration: none;
+          }
+          .nav_bar{
+          font-family: 'poppins';
+          font-size:20px;
+          float: right;
+          List-style: none;
+          }
+          .nav_bar li {
+          display: inline-block;
+          }
+          .nav_bar li a{
+          padding: 15px;
+          }
+          
+
+<h1><p>next code coming soon</h1></p>
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</HTML>
